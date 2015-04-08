ed26cbe0c1027dc6155b77353f86be108f036b93
ArpoJul <julio.polo.arias@gmail.com>
2015-04-08 14:35:41 -0500

    Initial Commit

---
+++ MultiplyTables/Multiply.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplyTables
+{
+    class Multiply
+    {
+        public void CreateTables()
+        {
+            Console.Write("\t 1\t 2\t 3\t 4\t 5\t 6\t 7\t 8\t 9");
+            for (int i = 1; i < 10; i++)
+            {
+                Console.Write("\t{0,2}", i);
+                for (int j = 1; j < 10; j++)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write("\t{0,2}", i * j);
+                }
+                
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("");
+            Console.WriteLine("Developed by: Julio Polo");
+            Console.WriteLine("Twitter : @Arpojul");
+        }
+    }
+}
---
+++ MultiplyTables/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplyTables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Multiply MyTables = new Multiply();
+
+            MyTables.CreateTables();
+
+            Console.WriteLine("\n\nPress any key to exit");
+            Console.ReadLine();
+        }
+    }
+}
